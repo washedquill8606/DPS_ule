490507ee623ee4d697f08706c20950a18afd3f20
Alex Puga <scdo117ds@outlook.com>
2024-10-30 15:44:10 +0100

    variable atomica

--- concurrency.c
+++ concurrency.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <threads.h>
+#include <stdatomic.h>
 
-int contador = 0;
+atomic_int contador = 0;
 
 int incrementar(void *arg) {
-    for (int i = 0; i < 100; ++i) {
-        contador++;
+    for (int i = 0; i < 100000; ++i) {
+        atomic_fetch_add(&contador, 1);
     }
+    return 0;
+}
+
+int main (void) {
+    thrd_t hilo1, hilo2;
+
+    thrd_create(&hilo1, incrementar, NULL);
+    thrd_create(&hilo2, incrementar, NULL);
+
+    thrd_join(hilo1, NULL);
+    thrd_join(hilo2, NULL);
+
+    printf("Contador final: %d\n", contador);
+
     return 0;
 }
